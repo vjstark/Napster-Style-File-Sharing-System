fce8d27a5a07c9be8381dff92f44553013b34797
vjstark <viratjoshi9@gmail.com>
2019-02-24 11:38:42 -0600

    a

---
+++ Consistency copy/test_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+JAVA_HOME=/usr/bin/java/
+CLASSPATH=/Users/vjstark/Documents/Consistency/GnutellaP2P_ios.jar:/Users/vjstark/Documents/Consistency/javax.ws.rs-api-2.0.jar:.
+$JAVA_HOME -cp $CLASSPATH com.gfiletransfer.MultiClient
